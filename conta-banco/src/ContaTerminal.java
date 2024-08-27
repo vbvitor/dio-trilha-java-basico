f501aebe461b5ba1a6e2a3a4481dffa52a5b479d
vitor <vitor.vbc19@gmail.com>
2024-08-26 23:14:09 -0300

    teste código 2

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,24 +2,22 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        //TODO:Conhecer e importar a classe Scanner
-        //Exibir as mensagens para o nosso usuário
-        //Obter pela classe Scanner os valores digitados no terminal
-        //Exibir a mensagem final
-    
-            Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Seja bem-vindo(a) ao Banco Continente. Para iniciar o atendimento, por favor digite o número da sua agência: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bem-vindo(a) ao Banco Continente! Para iniciar seu atendimento, por favor digite o número da sua agência: ");
         String agencia = scanner.nextLine();
 
-        System.out.print("Agora digite o número da sua conta com dígito: ");
-        String conta = scanner.nextLine();
+        System.out.print("Digite o número da sua conta: ");
+        int conta = scanner.nextInt();
+        scanner.nextLine();
 
-        System.out.print("Digite seu nome completo: ");
+        System.out.print("Agora, digite seu nome completo: ");
         String nome = scanner.nextLine();
 
-        System.out.print("E para finalizar, digite o seu saldo: ");
-        String saldo = scanner.nextLine();
+        System.out.print("Digite seu saldo: ");
+        double saldo = scanner.nextDouble();
 
         System.out.println("Olá, " + nome + ", obrigado por abrir uma conta no Banco Continente! Sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível.");
 
