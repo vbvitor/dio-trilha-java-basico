51f7efe03c4ea4584f03a3c2499070f3d5ba5e0f
vitor <vitor.vbc19@gmail.com>
2024-08-26 22:36:23 -0300

    teste de código 1

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO:Conhecer e importar a classe Scanner
+        //Exibir as mensagens para o nosso usuário
+        //Obter pela classe Scanner os valores digitados no terminal
+        //Exibir a mensagem final
+    
+            Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Seja bem-vindo(a) ao Banco Continente. Para iniciar o atendimento, por favor digite o número da sua agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Agora digite o número da sua conta com dígito: ");
+        String conta = scanner.nextLine();
+
+        System.out.print("Digite seu nome completo: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("E para finalizar, digite o seu saldo: ");
+        String saldo = scanner.nextLine();
+
+        System.out.println("Olá, " + nome + ", obrigado por abrir uma conta no Banco Continental! Sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível.");
+
+        scanner.close();
     }
 }
